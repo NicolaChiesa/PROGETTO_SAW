a7696acc5604862bfadf359b16185d0d5a4d5c0c
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-01-18 16:21:31 +0100

    Delete connessione.php

--- connessione.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$con = new mysqli('localhost', 'MIkig', 'Ninhothebest4', 'saw');
-	if (mysqli_connect_errno($con)) {
-	echo "Erore inserimanto utente: " . mysqli_connect_error($con);
-	}
-?>
