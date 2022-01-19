b30160c5a27e7b9c30bf7063b56f4a0f3f0facf5
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-01-19 17:33:58 +0100

    Update connessione.php

--- es10/connessione.php
+++ es10/connessione.php
@@ -1,11 +1,6 @@
 <?php
 	$con = new mysqli('localhost', 'gruppo', 'pippo', 'saw');
-<<<<<<< HEAD
 	if ($con->connect_errno) {
 	echo "Erore inserimanto utente: " .mysqli_connect_error($con);
-=======
-	if (mysqli_connect_errno($con)) {
-	echo "Errore inserimento utente: " . mysqli_connect_error($con);
->>>>>>> 6ad3b3d2960891e9c6eff2a5ac2643eb5826b79f
 	}
 ?>
