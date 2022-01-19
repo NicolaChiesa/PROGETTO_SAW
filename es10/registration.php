77cf2dea1406e11fddaa8572d008302d88b85f2f
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-01-19 17:54:43 +0100

    ivan
    tolto tutte le cose relative al codice fiscale da registration.php

--- es10/registration.php
+++ es10/registration.php
@@ -13,17 +13,12 @@
 	$row = mysqli_fetch_assoc($re);
 	$num = mysqli_affected_rows($con);
 	mysqli_free_result($re);
-	$queryy="SELECT* FROM utenti WHERE Codice='".$_POST['codice']."'";
-	$ress = mysqli_query($con,$queryy);
-	$row = mysqli_fetch_assoc($ress);
 	$numi = mysqli_affected_rows($con);
 	$pattern="/^[A-Z]{6,6}+[0-9]{2,2}+[A-Z]{1,1}+[0-9]{2,2}+[A-Z]{1,1}+[0-9]{3,3}+[A-Z]{1,1}$/";
 	if($_POST['pass']==""||$_POST['confirm']==""||$_POST['lastname']==""||$_POST['firstname']==""||$_POST['email']=="")
 		echo"<h1>mancano dati</h1>";
 	else if($_POST['pass']!=$_POST['confirm'])
 		echo"<h1>PASSWORD SABGLIATA</h1>";
-	else if(!preg_match($pattern,$_POST['codice']))
-		echo "<h1>Codice fiscale errato</h1>";
 	else if($num==1)
 		echo "<h1> email gi� usata</h1>";
 	else if($numi==1)
@@ -33,17 +28,15 @@
 	$lastname=htmlspecialchars($_POST['lastname']);
 	$firstname=htmlspecialchars($_POST['firstname']);
 	$email=htmlspecialchars($_POST['email']);
-	$codice=htmlspecialchars($_POST['codice']);
 	$pass=mysqli_real_escape_string($con, $pass);
 	$lastname=mysqli_real_escape_string($con, $lastname);
 	$firstname=mysqli_real_escape_string($con, $firstname);
 	$email=mysqli_real_escape_string($con, $email);
-	$codice=mysqli_real_escape_string($con, $codice);
 	$pass=password_hash($pass,PASSWORD_BCRYPT);
-	$query ="INSERT INTO utenti (Nome, Cognome, Password, Mail, Codice) VALUES ('".$firstname."', '".$lastname."', '".$pass."', '".$email."', '".$codice."')";
+	$query ="INSERT INTO utenti (Nome, Cognome, Password, Mail) VALUES ('".$firstname."', '".$lastname."', '".$pass."', '".$email."')";
 	$res = mysqli_query($con,$query);
 	mysqli_close($con);
-	echo "Inserimanto utente";
+	echo "Inserimento utente";
 	}
 
 	/****************************************************************************/
