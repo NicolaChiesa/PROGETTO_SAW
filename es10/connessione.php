6ad3b3d2960891e9c6eff2a5ac2643eb5826b79f
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-01-19 16:59:11 +0100

    Update connessione.php

--- es10/connessione.php
+++ es10/connessione.php
@@ -1,6 +1,6 @@
 <?php
 	$con = new mysqli('localhost', 'gruppo', 'pippo', 'saw');
-	if (mysqli_connect_error($con)) {
+	if (mysqli_connect_errno($con)) {
 	echo "Errore inserimento utente: " . mysqli_connect_error($con);
 	}
 ?>
