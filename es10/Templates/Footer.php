5d66e66e2eb2ea446cf595bc9a648fd381ebe202
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-01-21 19:48:47 +0100

    ivan
    ho "decorato " il footer

--- es10/Templates/Footer.php
+++ es10/Templates/Footer.php
@@ -1,3 +1,16 @@
-<footer> Copyright: the Universal Digital Library - Cookie policy - Feedback</footer>
+    <footer> 
+        <div class="footer-content">
+            <p>TITOLO/LOGO</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+        </div>  
+        <div class="footer-bottom">
+            <p> Copyright: the Universal Digital Library - Cookie policy - Feedback</p>
+        </div>          
+    </footer>
 </body>
 </html>
