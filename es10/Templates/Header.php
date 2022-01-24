c7a96affdf4c107ebfe2a2e6fdd23f7fee15e7e3
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-01-24 11:52:25 +0100

    Cambiato il nome del file degli stili in "Stile.css"

--- es10/Templates/Header.php
+++ es10/Templates/Header.php
@@ -9,7 +9,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Registration</title>
-    <link rel="stylesheet" type="text/css" href="../Stili/esercizio4.css">
+    <link rel="stylesheet" type="text/css" href="../Stili/Stile.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
