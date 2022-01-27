f9065be565a3ecd33fb79fd4deac48ecf42d00ec
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-01-27 09:56:55 +0100

    Update controlloEmail.php

--- es10/Registrazione/controlloEmail.php
+++ es10/Registrazione/controlloEmail.php
@@ -1,7 +1,7 @@
 <?php
 	include('../connessione.php');
-
-   /* $comando = "select Nome from utenti where Mail ='" .$_GET["email"] . "'";
+   
+    /*$comando = "select Nome from utenti where Mail ='" .$_GET["email"] . "'";
     $result = mysqli_query($con, $comando);
 
     if ( mysqli_num_rows($result)>0)
@@ -11,8 +11,7 @@
     else{
         echo "non trovata";
     }
-    */
-
+      */
 
     $("#emailutente").keyup(function() {
   var dati = $('#emailutente').attr('value');
@@ -35,5 +34,6 @@
     }
   });
 });
+
     mysqli_close($con);
 ?>
