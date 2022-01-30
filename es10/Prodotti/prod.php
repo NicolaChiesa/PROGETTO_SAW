36133c38b351b53247775b240f526c8d806335ce
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-01-30 17:30:37 +0100

    Create prod.php

---
+++ es10/Prodotti/prod.php
@@ -0,0 +1,32 @@
+<?php
+  include('../Templates/Header.php');
+?>
+<div>
+  <p>QUESTA E' LA PAGINA PRINCIPALE</p>
+  <p>After registration you will be able to access several services </p>
+</div>
+  <div>
+  <p>METTEREI VARIE FOTO DEI PRODOTTI CON IL NOME E CLICCANDO SULL'IMMAGINE DEL PRODOTTO MANDA ALLA PAGINA PER AGGIUNGERLO AL CARRELLO'</p>
+  <p><a href="anello.php" target="_self"> 
+   <img src="../Immagini/anellodelpotere.jpg" alt="Tutorials Point" border="0"/> 
+</a></p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+
+  </div>
+
+  <div>
+     <ul> 
+	<li>Cerca ogge</li>
+	<li>Compra un oggetto</li>
+     </ul>
+  </div>
+
+</div>
+<?php
+  include('../Templates/Footer.php');
+?>
