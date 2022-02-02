6f8a4d4edda653c5de99353d51a76530491a141e
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-02-02 12:27:07 +0100

    Update Footer.php
    piccole modifiche di indentazione

--- es10/Templates/Footer.php
+++ es10/Templates/Footer.php
@@ -1,19 +1,19 @@
 <div>
-    <footer class="py-5 px-5 bg-dark position-relative" >
-      <div class="row">
-        <div class="col-7">
-          <h5 class="text-light">Sezioni</h5>
-          <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="../Core/PaginaPrincipale.php" class="nav-link p-0 text-muted">Home</a></li>
-            <li class="nav-item mb-2"><a href="../Prodotti/prod.php" class="nav-link p-0 text-muted">Articoli</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Novità</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Chi siamo</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contatti</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          </ul>
-        </div>
+  <footer class="py-5 px-5 bg-dark position-relative" >
+    <div class="row">
+      <div class="col-7">
+        <h5 class="text-light">Sezioni</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="../Core/PaginaPrincipale.php" class="nav-link p-0 text-muted">Home</a></li>
+          <li class="nav-item mb-2"><a href="../Prodotti/prod.php" class="nav-link p-0 text-muted">Articoli</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Novità</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Chi siamo</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contatti</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        </ul>
+      </div>
   
-        <div class="col-4 offset-1">
+      <div class="col-4 offset-1">
         <?php   
         if(isset($_SESSION['Registrated']) && $_SESSION['Registrated'] == "true"){
             echo'<form id="myform">
@@ -26,20 +26,20 @@
           </form>
         </div>';
         }
-
-    ?> 
+        ?> 
       </div>
+    </div>
   
-      <div class="d-flex justify-content-between py-4 border-top text-light">
-        <p>&copy; 2021 Company, Only Mytich, tutti i diritti sono riesrvati</p>
-        <ul class="list-unstyled d-flex">
-          <li class="ms-3"><a class="icone" href="https://www.instagram.com/"><i class="fab fa-instagram-square"></i></a></li>
-          <li class="ms-3"><a class="icone" href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a></li>
-          <li class="ms-3"><a class="icone" href="https://twitter.com/"><i class="fab fa-twitter-square"></i></a></li>
-        </ul>
-      </div>
-    </footer>
-  </div>
+    <div class="d-flex justify-content-between py-4 border-top text-light">
+      <p>&copy; 2021 Company, Only Mytich, tutti i diritti sono riesrvati</p>
+      <ul class="list-unstyled d-flex">
+        <li class="ms-3"><a class="icone" href="https://www.instagram.com/"><i class="fab fa-instagram-square"></i></a></li>
+        <li class="ms-3"><a class="icone" href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a></li>
+        <li class="ms-3"><a class="icone" href="https://twitter.com/"><i class="fab fa-twitter-square"></i></a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
