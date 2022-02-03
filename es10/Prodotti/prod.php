77b80863858763fbe85c17bd9847e4360afc074c
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-02-03 19:49:26 +0100

    ivan

--- es10/Prodotti/prod.php
+++ es10/Prodotti/prod.php
@@ -4,7 +4,7 @@
 <div class="container py-5">
   <div class="row row-cols-1 row-cols-md-3 g-4">
     <div class="col">
-      <div class="card h-100">
+      <div class="card border-dark h-100">
         <img src="../Immagini/anellodelpotere2.jpg" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -14,7 +14,7 @@
       </div>
     </div>
     <div class="col">
-      <div class="card h-100">
+      <div class="card border-dark h-100">
         <img src="../Immagini/mjolnir.png" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -24,7 +24,7 @@
       </div>
     </div>
     <div class="col">
-      <div class="card h-100">
+      <div class="card border-dark h-100">
         <img src="../Immagini/excalibur2.png" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -34,7 +34,7 @@
       </div>
     </div>
     <div class="col">
-      <div class="card h-100">
+      <div class="card border-dark h-100">
         <img src="../Immagini/olifante.jpg" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -44,7 +44,7 @@
       </div>
     </div>
     <div class="col">
-      <div class="card h-100">
+      <div class="card border-dark h-100">
         <img src="../Immagini/pandora.jpg" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -54,7 +54,7 @@
       </div>
     </div>
     <div class="col">
-      <div class="card h-100"> 
+      <div class="card border-dark h-100"> 
         <img src="../Immagini/durlindana.jpg" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -64,7 +64,7 @@
       </div>
     </div>
     <div class="col">
-      <div class="card h-100">
+      <div class="card border-dark h-100">
         <img src="../Immagini/graal.jpg" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
@@ -74,8 +74,18 @@
       </div>
     </div>
     <div class="col">
-      <div class="card h-100">
+      <div class="card border-dark h-100">
         <img src="../Immagini/pomo.jpg" class="card-img-top immagini" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card border-dark h-100">
+        <img src="../Immagini/RuyiJinguBang3.jpg" class="card-img-top immagini" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
