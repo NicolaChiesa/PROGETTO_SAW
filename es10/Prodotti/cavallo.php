05e6a48471dfbb69687a991a2f4f671051f00a01
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-03 16:47:17 +0100

    Create cavallo.php

---
+++ es10/Prodotti/cavallo.php
@@ -0,0 +1,34 @@
+<?php
+  include('../Templates/Header.php');
+  $_SESSION['prodotto']='Cavallo di Troia';
+  $_SESSION['IDprodotto']=3;
+?>
+<div>
+  <p>QUESTA E' LA PAGINA PRINCIPALE</p>
+
+  <a href="carrello.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>
+
+</div>
+  <div>
+  <p>Anello del potere</p>
+  <p>ff</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+
+  </div>
+
+  <div>
+     <ul> 
+	<li>Cerca ogge</li>
+	<li>Compra un oggetto</li>
+     </ul>
+  </div>
+
+</div>
+<?php
+  include('../Templates/Footer.php');
+?>
