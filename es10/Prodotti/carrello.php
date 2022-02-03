68ca777b4e83efc06da8990bf096eedb8c6cc0df
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-03 13:30:45 +0100

    Update carrello.php

--- es10/Prodotti/carrello.php
+++ es10/Prodotti/carrello.php
@@ -1,20 +1,21 @@
 <?php
 	include('../Templates/Header.php');
 	include('../connessione.php');
-	$query = "SELECT prezzo, quantita FROM prodotti WHERE Nome='".$_SESSION['prodotto']."'";
+	$query = "SELECT prezzo, quantita FROM prodotti WHERE Nome='".$prodotto."'";
 	$res=mysqli_query($con,$query);
 	$row = mysqli_fetch_assoc($res);
 	if($row['quantita']>0)
 		{
 		$row['quantita']-=1;
-		$quer="UPDATE `prodotti` SET `quantita` = '".$row['quantita']."' WHERE `Nome` = '".$_SESSION['prodotto']."'";
+		$quer="UPDATE `prodotti` SET `quantita` = '".$row['quantita']."' WHERE `Nome` = '".$prodotto."'";
 		$re = mysqli_query($con,$quer);
-		echo 'Stai acquistando un: '.$_SESSION['prodotto'].' alla modica cifra di: '.$row['prezzo'].' euro';
+		$querys="INSERT INTO `acquisto` (NomeProdotto, session_id, quantita, IDutente, IDprodotto),('".$prodotto."', session_id(), 1,'".$_SESSION['id']."', '".$IDprodotto."')";
+		$ress = mysqli_query($con,$querys);
 		//header("Location: prod.php");
 		}
 	else
 		{
-		header("Location: prod.php");
+		//header("Location: prod.php");
 		echo 'Non disponibile in magazzino';
 		}
 	mysqli_close($con);	
