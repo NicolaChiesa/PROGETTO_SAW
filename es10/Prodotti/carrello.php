745104220a256f640faedd5c324e8e6597c2a4c0
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-03 13:45:33 +0100

    Update carrello.php

--- es10/Prodotti/carrello.php
+++ es10/Prodotti/carrello.php
@@ -1,16 +1,21 @@
 <?php
 	include('../Templates/Header.php');
 	include('../connessione.php');
-	$query = "SELECT prezzo, quantita FROM prodotti WHERE Nome='".$prodotto."'";
+	$query = "SELECT prezzo, quantita FROM prodotti WHERE Nome='".$_SESSION['prodotto']."'";
 	$res=mysqli_query($con,$query);
 	$row = mysqli_fetch_assoc($res);
 	if($row['quantita']>0)
 		{
 		$row['quantita']-=1;
-		$quer="UPDATE `prodotti` SET `quantita` = '".$row['quantita']."' WHERE `Nome` = '".$prodotto."'";
+		$quer="UPDATE `prodotti` SET `quantita` = '".$row['quantita']."' WHERE `Nome` = '".$_SESSION['prodotto']."'";
 		$re = mysqli_query($con,$quer);
-		$querys="INSERT INTO `acquisto` (NomeProdotto, session_id, quantita, IDutente, IDprodotto),('".$prodotto."', session_id(), 1,'".$_SESSION['id']."', '".$IDprodotto."')";
+		if(isset($_SESSION['Registrated']) && $_SESSION['Registrated'] == "true"){
+			$querys="INSERT INTO `acquisto` (NomeProdotto, session_id, quantita, IDutente, IDprodotto),('".$_SESSION['prodotto']."', session_id(), 1,'".$_SESSION['id']."', '".$_SESSION['IDprodotto']."')";
+			}
+		else
+			$querys="INSERT INTO `acquisto` (NomeProdotto, session_id, quantita, IDutente, IDprodotto),('".$_SESSION['prodotto']."', session_id(), 1,0, '".$_SESSION['IDprodotto']."')";
 		$ress = mysqli_query($con,$querys);
+		$num = mysqli_affected_rows($con);
 		//header("Location: prod.php");
 		}
 	else
