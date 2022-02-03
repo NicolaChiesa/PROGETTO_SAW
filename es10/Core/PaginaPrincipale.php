49416d5eff6b01366b6b3c60d9f244504d507ac1
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-02-04 00:19:15 +0100

    ivan

--- es10/Core/PaginaPrincipale.php
+++ es10/Core/PaginaPrincipale.php
@@ -31,15 +31,15 @@
         <legend class="scritta">About Us</legend>
         <p class="testoBase">
                 Eccoci giunti a noi, ti starai chiedendo chi siamo, e qual é il nostro obbiettivo, beh siamo qui per dar
-                ti una risposta! Siamo una Start Up nata a Genova nel 2021 con l obbiettivo di portare le reraviglie 
+                la risposta! Siamo una Start Up nata a Genova nel 2021 con l'obbiettivo di portare le meraviglie 
                 mitologiche a casa di tutti! Purtoppo in poche persone colgono la bellezza di questi oggetti... <br><br>
                 La loro storia, come il loro utilizzo sono un'esperienza unica, la bellezza che possono portare nelle 
                 nostre case é fantastica. Danno quel tocco di stile antico ma allo stesso tempo moderno, perché 
-                come vediamo anche nella moda le fantasie a tema mitologico stanno nascendo sempre di piú <br><br>
-                Per cui se vuoi stupire i tuoi ospiti e mostrarti come una persona che ha occhio per la bellezza e il design
+                come vediamo anche nella moda le fantasie a tema mitologico stanno ritornando sempre di piú. <br><br>
+                Perciò se vuoi stupire i tuoi ospiti e mostrarti come una persona che ha occhio per la bellezza e il design
                 sei nel posto giusto, non ti resta che tovare il prodotto che piú si addice al tuo salotto! Che aspetti!<br><br>
                 Se vuoi rimanere sempre aggiornato sullà aggiunta di nuovi prodotti non devi far altro che 
-                iscriverti alla nostra newsletter qua sotto !
+                iscriverti alla nostra newsletter, tramite la sezione qua sotto !
         </p>
     </fieldset>  
   </div>
