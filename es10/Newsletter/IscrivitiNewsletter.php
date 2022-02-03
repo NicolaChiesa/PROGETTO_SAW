e5690fff12ea808a8c26d25b245e89ad165dc5df
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-03 10:37:18 +0100

    Update IscrivitiNewsletter.php

--- es10/Newsletter/IscrivitiNewsletter.php
+++ es10/Newsletter/IscrivitiNewsletter.php
@@ -1,11 +1,11 @@
 <?php
 	include('../Templates/Header.php');
 	include('../connessione.php');
-	$email=mysqli_real_escape_string($con, $_SESSION['email']);
-	$query = "UPDATE utenti SET news=1 WHERE Mail='".$email."'";
+	$email=mysqli_real_escape_string($con, $_POST['email']);
+	$query = "INSERT INTO newsletters(email, news) VALUES ('".$email."',1)";
 	$res=mysqli_query($con,$query);
 	mysqli_close($con);
-	header("Location: SendEmailConfirm.php");
+	//header("Location: SendEmailConfirm.php");
 ?>
 
 </body>
