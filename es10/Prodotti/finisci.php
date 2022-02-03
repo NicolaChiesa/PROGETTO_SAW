5ba7de2a495f4d9946e5135c729045ec7acb86b0
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-03 17:14:24 +0100

    Create finisci.php

---
+++ es10/Prodotti/finisci.php
@@ -0,0 +1,24 @@
+<?php
+	include('../Templates/Header.php');
+	include('../connessione.php');
+	$query = "SELECT * FROM acquisto WHERE NumSessione='".session_id()."'";
+	$res=mysqli_query($con,$query);
+	$row = mysqli_fetch_assoc($res);
+	$rowcount=mysqli_num_rows($res);
+	if($rowcount>0)
+		{
+		echo 'Stai acquistando dal nostro sito: '.$row['NomeProdotto'].' alla modica cifra di: '.$row['Prezzo'];
+		$prezzo=$row['Prezzo'];
+		}
+	while($row=mysqli_fetch_assoc($res))
+		{
+		echo' acquisti anche: '.$row['NomeProdotto'].' alla modica cifra di: '.$row['Prezzo'];
+		$prezzo+=$row['Prezzo'];
+		}
+		echo ' per una spesa totale di: '.$prezzo;
+	mysqli_close($con);	
+	include('../Templates/Footer.php');
+?>
+
+</body>
+</html>
