b5902f372eaa956de09646f0ebfdf940fad77f89
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-03 10:10:32 +0100

    lavorato su pag principale

--- es10/Core/PaginaPrincipale.php
+++ es10/Core/PaginaPrincipale.php
@@ -6,7 +6,7 @@
   <p>After registration you will be able to access several services </p>
    <p><a href="reserved.php">Vai alla pagina riservata</a></p>
 </div>
-  <div>
+<div>
   <p>QUA CI VA LA PRESENTAZIONE DEL SITO E SU COSA FA</p>
   <p>ff</p>
   <p>FA</p>
@@ -15,17 +15,53 @@
   <p>FA</p>
   <p>FA</p>
   <p>FA</p>
+</div>
+<div class="mydiv2">
+<div class="mydiv2">
 
-  </div>
+<div class="mydiv1">
+    <p class="scritta">L'agenda che permette <br> di sincronizzarsi <br>con i propri amici</p>
+    <img src="../Immagini/graal.jpg" alt="Plan con gli amici" height="400"/>
+</div>
 
-  <div>
+<div class = "mydiv1">
+    <img src="svg/Organizza.svg" alt="Organizza online" height="370" />
+    <p class="scritta">Con un semplice click <br> si avrà una pianificazione <br> perfetta degli impegni</p>
+</div>
+
+<div class = "mydiv1">
+    <p class="scritta">Inoltre è la prima <br> agenda che coordina<br>sia il proprio tempo che <br>i propri spazi</p>
+    <img src="svg/list.svg" alt="To-Do List" height="370" />
+</div>
+
+<fieldset class="descrizione">
+    <legend class="scritta">About Us</legend>
+
+    <p class="about">
+            PlanIt vuole offrire la possibilità di sincronizzare i propri impegni con quelli 
+        degli amici o anche colleghi. Inizialmente era stata pensata principalmente per migliorare 
+        la convivenza tra coinquilini, dato che c'è la possibilità di segnare un possibile evento
+        e in questo modo avvisare anche i diretti interessati.<br><br>
+            Ci si potrebbe però chiedere che cosa abbia di diverso dalle altre agende che permettono ad esempio 
+        già di condividere eventi con determinate persone...<br><br>
+            PlanIt è stata ideata pensando a uno sviluppo futuro nel campo IoT. Infatti quello che si vuole 
+        raggiungere è un connubio tra organizzazione del tempo e dello spazio.<br><br>
+            Questo sarebbe permesso, ad esempio, tramite l'acquisti dell'upgrade "Casa" che oltre ad attivare 
+        le funzioni non presenti nella versione base (Prenotazione di uno spazio, turni cucina, etc) può spedire, se 
+        richiesto dall'utente, un set base IoT con una serratura intelligente che garantisce la propria privacy
+    </p>
+    
+</fieldset>  
+
+</div>
+
+<div>
      <ul> 
 	<li>Cerca ogge</li>
 	<li>Compra un oggetto</li>
      </ul>
-  </div>
-
 </div>
+
 <?php
   include('../Templates/Footer.php');
 ?>
