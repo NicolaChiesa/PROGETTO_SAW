5bd1cd6b4d9e0c0f4d4cb26945ecade5416b7c76
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-03 10:10:05 +0100

    Rese piú belle le pagine riservate

--- es10/Core/NonAutenticato.php
+++ es10/Core/NonAutenticato.php
@@ -1,10 +1,12 @@
 <?php
 include('../Templates/Header.php');
 
-echo "Ci dispiace ma prima di accedere a questa pagina devi autenticarti tramite il login, sei pregato di 
-        tornare alla home e accedere prima di provare ad entrare in questa pagina";
-
+echo"   <center>
+        <div class=\"DataDiv\">
+	<p class=\"testoMessage\"> Ci dispiace ma prima di accedere a questa pagina devi autenticarti tramite il login, sei pregato di 
+        tornare alla home e accedere prima di provare ad entrare in questa pagina</p>
+        </div>
+        </center>"; 
 
 include('../Templates/Footer.php');
-
 ?>
--- es10/Core/reserved.php
+++ es10/Core/reserved.php
@@ -1,13 +1,20 @@
+
 <?php
 include('../Templates/Header.php');
 if (!($_SESSION['Registrated'] == "true")){
 	header("Location: NonAutenticato.php");
 }
-else
-	echo "Benvenuto";
+else{
+	echo"
+	<center>
+	 <div class=\"DataDiv\">
+	  <p class=\"testoMessage\"> 
+	  		Benvenuto nella pagina riservata, stiamo ancora lavorando <br> 
+			per far si che qui tu possa trovare un contenuto su misura per  <br>
+			te, iscriviti alla newsletter per veniure subito informato per quando sará pronta!! <br></p>
+	 </div>
+	</center>";
+}
 
-?>
-<h1> PAGINA RISERVATA </h1>
-<?php
   include('../Templates/Footer.php');
 ?>
