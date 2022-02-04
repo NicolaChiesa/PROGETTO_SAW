a58607d8d86cf42e2e033eecee0530d2d9938737
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-04 10:23:48 +0100

    email

--- es10/Newsletter/IscrivitiNewsletter.php
+++ es10/Newsletter/IscrivitiNewsletter.php
@@ -1,11 +1,15 @@
 <?php
 	include('../Templates/Header.php');
 	include('../connessione.php');
-	$email=mysqli_real_escape_string($con, $_POST['email']);
+	if(isset($_POST['email'])){
+		$email=mysqli_real_escape_string($con, $_POST['email']);
+	} else{
+		$email=$_SESSION['email'];
+	}
 	$query = "INSERT INTO newsletters(email, news) VALUES ('".$email."',1)";
 	$res=mysqli_query($con,$query);
 	mysqli_close($con);
-	$_SESSION['email']=$_POST['email'];
+	
 	header("Location: SendEmailConfirm.php");
 ?>
 
