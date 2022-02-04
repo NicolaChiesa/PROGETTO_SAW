b28e46b19199a6ace86dd5a1a22ee1e1101d7a3b
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-04 17:09:07 +0100

    Create IscrivitiNewsletter.php

---
+++ PROGETTO_Chiesa_Greco_Terrile/Newsletter/IscrivitiNewsletter.php
@@ -0,0 +1,23 @@
+<?php
+	include('../Templates/Header.php');
+	include('../connessione.php');
+	
+	if(!isset($_SESSION['nome'])){
+		$email=mysqli_real_escape_string($con, $_POST['email']);
+		$_SESSION['email']=$email;
+	}
+	$query = "INSERT INTO newsletters(email, news) VALUES ('".$email."',1)";
+	$res=mysqli_query($con,$query);
+	mysqli_close($con);
+	
+	header("Location: SendEmailConfirm.php");
+?>
+
+</body>
+</html>
+
+Notice: Undefined index: email in /chroot/home/S4825218/public_html/es10/Newsletter/SendEmailConfirm.php on line 35
+Message could not be sent. Mailer Error: Invalid address: (cc):
+
+Notice: Undefined index: email in /chroot/home/S4825218/public_html/es10/Newsletter/SendEmailConfirm.php on line 35
+Message could not be sent. Mailer Error: Invalid address: (cc):
