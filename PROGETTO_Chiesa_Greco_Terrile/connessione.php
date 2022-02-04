ad6d9f9e352418d5d399d95eb6387df84852be18
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-04 18:29:08 +0100

    Update connessione.php

--- PROGETTO_Chiesa_Greco_Terrile/connessione.php
+++ PROGETTO_Chiesa_Greco_Terrile/connessione.php
@@ -1,5 +1,5 @@
 <?php
-	$con = new mysqli('localhost', 'gruppo', 'pippo', 'saw');
+	$con = new mysqli('localhost', 'S4825218', 'Celafaremo1', 'S4825218');
 	if ($con->connect_errno) {
 	echo "Erore inserimento utente: " .mysqli_connect_error($con);
 	}
