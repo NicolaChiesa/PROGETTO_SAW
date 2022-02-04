fa04c6022e63b9e089949d38e9ee48d99c4df224
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-04 10:35:06 +0100

    mail

--- es10/Newsletter/IscrivitiNewsletter.php
+++ es10/Newsletter/IscrivitiNewsletter.php
@@ -1,10 +1,10 @@
 <?php
 	include('../Templates/Header.php');
 	include('../connessione.php');
-	if(isset($_POST['email'])){
-		$email=mysqli_real_escape_string($con, $_POST['email']);
-	} else{
+	if(isset($_SESSION['nome'])){
 		$email=$_SESSION['email'];
+	} else{
+		$email=mysqli_real_escape_string($con, $_POST['email']);
 	}
 	$query = "INSERT INTO newsletters(email, news) VALUES ('".$email."',1)";
 	$res=mysqli_query($con,$query);
@@ -15,3 +15,6 @@
 
 </body>
 </html>
+
+Notice: Undefined index: email in /chroot/home/S4825218/public_html/es10/Newsletter/SendEmailConfirm.php on line 35
+Message could not be sent. Mailer Error: Invalid address: (cc):
