7d53c2885f4814b540e00adfce67a96700c64575
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-04 17:09:17 +0100

    Create anello.php

---
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/anello.php
@@ -0,0 +1,35 @@
+<?php
+  include('../Templates/Header.php');
+  $_SESSION['prodotto']='Anello del Potere';
+  $_SESSION['IDprodotto']=1;
+?>
+<div>
+  <p>QUESTA E' LA PAGINA PRINCIPALE</p>
+
+  <a href="carrello.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Aggiungi al carrello</button></a>
+  <a href="finisci.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>
+
+</div>
+  <div>
+  <p>Anello del potere</p>
+  <p>ff</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+  <p>FA</p>
+
+  </div>
+
+  <div>
+     <ul> 
+	<li>Cerca ogge</li>
+	<li>Compra un oggetto</li>
+     </ul>
+  </div>
+
+</div>
+<?php
+  include('../Templates/Footer.php');
+?>
