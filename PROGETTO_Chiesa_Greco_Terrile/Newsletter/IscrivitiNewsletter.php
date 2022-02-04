b848bda6df3ee7560b0eb3acfdb3be9d070f1bad
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-04 18:12:59 +0100

    Update IscrivitiNewsletter.php

--- PROGETTO_Chiesa_Greco_Terrile/Newsletter/IscrivitiNewsletter.php
+++ PROGETTO_Chiesa_Greco_Terrile/Newsletter/IscrivitiNewsletter.php
@@ -3,13 +3,13 @@
 	include('../connessione.php');
 	
 	if(isset($_SESSION['Registrated'])){
-		$email = $_SESSION['email'];
+		//$email = $_SESSION['email'];
 	}
 	else{
-		$email=mysqli_real_escape_string($con, $_POST['email']);
+		$_SESSION['email']=mysqli_real_escape_string($con, $_POST['email']);
 	}
 	
-	$query = "INSERT INTO newsletters(email, news) VALUES ('".$email."',1)";
+	$query = "INSERT INTO newsletters(email, news) VALUES ('".$_SESSION['email']."',1)";
 	$res=mysqli_query($con,$query);
 	mysqli_close($con);
 	
