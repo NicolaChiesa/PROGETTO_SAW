62ebcfcca94bf79c9972e6011a2ac620f1baa2fa
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-04 17:08:57 +0100

    Create FormLogin.php

---
+++ PROGETTO_Chiesa_Greco_Terrile/Login/FormLogin.php
@@ -0,0 +1,22 @@
+<?php
+    include('../Templates/Header.php');
+?>
+
+<center>
+<div class="DataDiv"> 
+<legend class="testoTitolo">Effettua il Login</legend><br>
+  <form id="myform" action="login.php" method="POST"> 
+      <i class="fa fa-user" style="font-size:13px;color:rgba(65, 65, 65, 1.0)"></i>
+      <input type="email" id="email" class="no-outline"  name="email" placeholder="E-mail"><br>
+
+      <i class="fa fa-user" style="font-size:13px;color:rgba(65, 65, 65, 1.0)"></i>
+      <input type="password" class="no-outline" id="pass" name="pass" placeholder="Password"><br>
+	    <input type="submit" value="Login">
+      <br><br><br>
+  </form>
+</div>
+</center>
+
+<?php
+  include('../Templates/Footer.php');
+?>
