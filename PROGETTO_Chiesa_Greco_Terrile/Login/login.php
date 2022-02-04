a8e82c6ad343d3bc4098301e24f5da63f6325a7c
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-04 17:08:59 +0100

    Create login.php

---
+++ PROGETTO_Chiesa_Greco_Terrile/Login/login.php
@@ -0,0 +1,23 @@
+<?php
+	include('../Templates/Header.php');
+	include('../connessione.php');
+	$pass=trim($_POST['pass']);
+	$email=mysqli_real_escape_string($con, $_POST['email']);
+	$query = "SELECT Password, Nome, Cognome, ID FROM utenti WHERE Mail='".$email."'";
+	$res=mysqli_query($con,$query);
+	$row = mysqli_fetch_assoc($res);
+	if(password_verify($pass,$row['Password'])){
+		$_SESSION['nome']=$row['Nome'];
+		$_SESSION['cognome']=$row['Cognome'];
+		$_SESSION['id']=$row['ID'];
+		$_SESSION['Registrated'] = "true";
+		$_SESSION['email']= $_POST['email'];
+		header("Location: ../Core/PaginaPrincipale.php");
+		}
+	else
+		echo"ERRORE";
+	mysqli_close($con);
+?>
+
+</body>
+</html>
