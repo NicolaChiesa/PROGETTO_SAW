87b57ec0c56394386bf26d1e0baca170421ed8e1
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-04 17:09:05 +0100

    Create modifica.php

---
+++ PROGETTO_Chiesa_Greco_Terrile/ModificaUtente/modifica.php
@@ -0,0 +1,39 @@
+<?php
+include('../connessione.php');
+include('../Templates/Header.php');
+if (!isset($_SESSION['Registrated'])){
+	header("Location: ../Core/PaginaPrincipale.php");
+}
+else{
+    $nome=$_SESSION['nome'];
+	echo "<h1>Benvenuto nella pagina di modifica $nome</h1>";
+    }
+$nome=$_SESSION['nome'];
+$cognome=$_SESSION['cognome'];
+$mail=$_SESSION['email'];
+?>
+<?php
+$form='<center>
+<div class="DataDiv"> 
+
+<form id="myform" action="mod.php" method="POST"> 
+    <i class="fa fa-user" style="font-size:13px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="email" id="email" class="no-outline"  name="email" value='.$mail.'><br>
+
+    <i class="fa fa-user" style="font-size:13px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="text" id="firstname" class="no-outline"  name="firstname" value='.$nome.'><br>
+
+    <i class="fa fa-user" style="font-size:13px;color:rgba(65, 65, 65, 1.0)"></i>
+    <input type="text" id="lastname" class="no-outline"  name="lastname" value='.$cognome.'><br>
+
+	<input type="submit" value="MODIFICA">
+    <br><br><br>
+    </form>
+    </center>';
+    echo $form;
+    mysqli_close($con);
+    ?>
+</div>
+<?php
+  include('../Templates/Footer.php');
+?>
