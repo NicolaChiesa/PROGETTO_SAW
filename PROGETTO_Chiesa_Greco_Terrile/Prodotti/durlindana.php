c34d11e00f767be0434e281211c99edb562814ce
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-05 11:04:17 +0100

    sistemo prodotti

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
@@ -6,22 +6,31 @@
 <div class="container py-5">
   <div class="mydiv1">
     <div class="polaroid">
-      <img src="../Immagini/durlindana.jpg"  alt="..." height="600"/>
-     </div>
-          
-    <p class="testoBase">Le origini dell'arma non sono chiare, visto che il ciclo carolingio propone diverse tesi 
-      contrastanti, anche se la più conosciuta è che a forgiare la spada sia stato Weland il fabbro.<br> 
-      La Chanson de Roland vuole invece che la spada fosse stata donata a Orlando proprio da Carlo Magno, 
-      che l'avrebbe a sua volta ricevuta in dono da un angelo; <br>
-      nel famoso poema Orlando Furioso di Ludovico Ariosto, si dice invece che la spada sarebbe stata 
-      donata al cavaliere da Malagigi e che sarebbe un tempo appartenuta a Ettore di Troia 
-      (tuttavia non c'è nessuna menzione della spada nella Iliade). <br>
-    </p>
-
-  <p>PAGINA DURLINDANA</p>
+      <img src="../Immagini/durlindana.jpg"  alt="..." height="350"/>
+    </div>
+    <div>
+      <p class="titoloCard"> Durlindana </p>
+      <p class="testoBase">Le origini dell'arma non sono chiare, visto che il ciclo <br> 
+        carolingio propone diverse tesi contrastanti,<br>
+        anche se la più conosciuta è <br> 
+        che a forgiare la spada sia stato Weland il fabbro.<br> 
+        La Chanson de Roland vuole invece che la spada fosse stata <br> 
+        donata a Orlando proprio da Carlo Magno, <br> 
+        che l'avrebbe a sua volta ricevuta in dono da un angelo; <br>
+        nel famoso poema Orlando Furioso di Ludovico Ariosto, si dice invece <br> 
+        che la spada sarebbe stata donata al cavaliere da Malagigi e <br> 
+        che sarebbe un tempo appartenuta a Ettore di Troia <br> 
+        (tuttavia non c'è nessuna menzione della spada nella Iliade). <br>
+      </p>
+    </div>
+  </div>
+  <a href="carrello.php" <button type="button" class="btn btn-outline-dark me-2">Aggiungi al carrello</button></a>
+  <!--<a href="finisci.php" <button type="button" class="btn btn-outline-dark me-2">Vai al carrello</button></a>
   <a href="carrello.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Aggiungi al carrello</button></a>
-  <a href="finisci.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>
+  <a href="finisci.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>-->
+
 </div>
 <?php
   include('../Templates/Footer.php');
 ?>
+   
--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/mjolnir.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/mjolnir.php
@@ -3,10 +3,33 @@
   $_SESSION['prodotto']='Mjolnir';
   $_SESSION['IDprodotto']=3;
 ?>
+<div class="container py-5">
+  <div class="mydiv1">
+    <div class="polaroid">
+      <img src="../Immagini/mjolnir.png"  alt="..." height="350"/>
+    </div>
+    <div>
+      <p class="titoloCard"> Mjolnir </p>
+      <p class="testoBase">Il Mjöllnir, sovente semplificato in Mjolnir chiamato anche Martello di Thor, ,<br> 
+         è l'arma di Thor (lett. "Fulmine"), il dio personificante il fulmine<br> 
+         e il tuono nella mitologia norrena. È in genere rappresentato originariamente<br> 
+         come un martello. È realizzato dai nani, o, secondo l'Edda in prosa<br>  
+         di Snorri Sturluson del XIII secolo, dal nano Sindri per una disputa con <br> 
+         il dio Loki su chi avesse potuto creare l'arma più meravigliosa per gli dèi.<br> 
+         Il suo significato simbolico, teologico, è comparabile a quello del Vajra<br>
+         (il "fulmine" o "diamante", arma di Indra), e simboli simili, tra cui la svastica,<br> 
+         vedico come testimoniato dal fatto che in Scandinavia  <br>
+         e Inghilterra venisse rappresentato anche come croce uncinata
+         <br>
+      </p>
+    </div>
+  </div>
+  <a href="carrello.php" <button type="button" class="btn btn-outline-dark me-2">Aggiungi al carrello</button></a>
+  <!--<a href="finisci.php" <button type="button" class="btn btn-outline-dark me-2">Vai al carrello</button></a>
+  <a href="carrello.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Aggiungi al carrello</button></a>
+  <a href="finisci.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>-->
 
-<p>PAGINA MJOLNIR</p>
- <a href="carrello.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Aggiungi al carrello</button></a>
- <a href="finisci.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>
+</div>
 <?php
   include('../Templates/Footer.php');
 ?>
