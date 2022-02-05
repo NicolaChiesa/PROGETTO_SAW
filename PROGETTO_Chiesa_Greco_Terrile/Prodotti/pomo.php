90bcedc44b82757cb452f7ca7169d8464ffb0c42
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-05 12:37:01 +0100

    Update pomo.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/pomo.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/pomo.php
@@ -3,10 +3,26 @@
   $_SESSION['prodotto']='Pomo della Discordia';
   $_SESSION['IDprodotto']=5;
 ?>
-
-<p>PAGINA POMO DELLA DISCORDIA</p>
- <a href="carrello.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Aggiungi al carrello</button></a>
- <a href="finisci.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>
+<div class="container py-5">
+  <div class="mydiv1">
+    <div class="polaroid">
+      <img src="../Immagini/pomo.jpg"  alt="..." height="200"/>
+    </div>
+    <div>
+      <p class="titoloCard"> Pomo d'oro </p>
+      <p class="testoBase">
+      Il pomo della discordia o mela della discordia è,<br> 
+      secondo il mito, la mela lanciata da Eris, dea della discordia,<br>
+       sul tavolo dove si stava svolgendo il banchetto in onore del <br>
+       matrimonio di Peleo e Teti che causò una lite tra le dee presenti<br>
+      e che condusse, per una serie di eventi, alla Guerra di Troia.<br>
+      In senso figurato, il pomo della discordia è un oggetto o un <br>
+      fatto che è causa di disaccordo, di contrasto e di lite fra le persone.<br>
+      </p>
+    </div>
+  </div>
+  <a href="carrello.php" <button type="button" class="btn btn-outline-dark me-2">Aggiungi al carrello</button></a>
+</div>
 <?php
   include('../Templates/Footer.php');
 ?>
