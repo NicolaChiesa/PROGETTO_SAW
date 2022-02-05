7c8667df98ed56321d5eb1244492e39522088848
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-02-05 15:14:55 +0100

    ivan

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
@@ -30,31 +30,33 @@
   <a href="finisci.php" <button type="button" class="btn btn-outline-dark me-2">Vai al carrello</button></a>
   <!--<a href="carrello.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Aggiungi al carrello</button></a>
   <a href="finisci.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al carrello</button></a>-->
-    <div id="votazione">
+
+  <div id="votazione">
     <form name="sent-rating" id="sent-rating">
-    <input type="radio" value="10" name="star" id="stella1" title="eccellente" class="costellazione">
-    <input type="radio" value="8" name="star"  id="stella2" title="ottimo" class="costellazione">
-    <input type="radio" value="6" name="star"  id="stella3" title="discreto" class="costellazione">
-    <input type="radio" value="4" name="star"  id="stella4" title="male" class="costellazione">
-    <input type="radio" value="2" name="star"  id="stella5" title="pessimo" class="costellazione">
+      <input type="radio" value="10" name="star" id="stella1" title="eccellente" class="costellazione">
+      <input type="radio" value="8" name="star"  id="stella2" title="ottimo" class="costellazione">
+      <input type="radio" value="6" name="star"  id="stella3" title="discreto" class="costellazione">
+      <input type="radio" value="4" name="star"  id="stella4" title="basso" class="costellazione">
+      <input type="radio" value="2" name="star"  id="stella5" title="pessimo" class="costellazione">
     </form>
   </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-     $(document).ready(function(){
-        $(".costellazione").click(function(){
-            alert('hhhhhhhhhhh');
-            var vot=(this).val();
-            $.ajax({
-                type:"POST";
-                url"../Prodotti/recensione.php",
-                data:{voto:voto},
-                dataType:"html"
-            });
-        });       
-     });
-    </script>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $(".costellazione").click(function(){
+          alert('hhhhhhhhhhh');
+          var vot=(this).val();
+          $.ajax({
+              type:"POST";
+              url"../Prodotti/recensione.php",
+              data: {voto: voto},
+              dataType:"html"
+          });
+      });       
+    });
+  </script>
+
 <?php
   include('../Templates/Footer.php');
 ?>
