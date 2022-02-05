f4b4f03f363f63a3bdee7a8d998bb8d9269b300a
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-02-05 12:49:36 +0100

    Update Header.php

--- PROGETTO_Chiesa_Greco_Terrile/Templates/Header.php
+++ PROGETTO_Chiesa_Greco_Terrile/Templates/Header.php
@@ -31,7 +31,7 @@ session_start();
                 <div class="container">
                     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                            <svg class="bi me-2" width="40" height="32" role="img"><img src="../Immagini/anellodelpotere4.jpg" alt="anello del potere"></svg>              
+                            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>              
                         </a>
                 
                         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -60,7 +60,7 @@ session_start();
                 <div class="container">
                     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                            <svg class="bi me-2" width="40" height="32" role="img"><img src="../Immagini/anellodelpotere4.jpg" alt="anello del potere"></svg>               
+                            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>               
                         </a>
                 
                         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
