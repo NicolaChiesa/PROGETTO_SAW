b12de5e776ae693af56804cdad9dae22044165ef
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-06 13:40:25 +0100

    Update recensione.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/recensione.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/recensione.php
@@ -2,34 +2,42 @@
 	include('../Templates/Header.php');
 	include('../connessione.php');
 	if(!isset($_SESSION['Registrated']))
-		{
-			echo" ciaooooooo";
-			$reg='Devi accedere prima di potere acquistare!!';
-		}
+			$reg='Devi accedere prima di potere fare una recensione!!';
+	else if(!isset($_POST['star']))
+		$reg='Devidare uuna valutazione valida!!';
 	else
 		{
-			echo" vediamo se va";
-		$query = "SELECT IDutente FROM acquistato WHERE IDprodotto='".$_SESSION['IDprodotto']."'";
+		$query = "SELECT IDutente, Votato FROM acquistato WHERE IDprodotto='".$_SESSION['IDprodotto']."' AND `IDutente` = '".$_SESSION['id']."'";
 		$res = mysqli_query($con,$query);
-		$row = mysqli_fetch_assoc($res);
-		$rowcount = mysqli_num_rows($res);
-		if($rowcount>0)
+		if($res!=false)
 			{
-			$rate=$_POST['voto'];
-			$select = "SELECT Voto, NumVoti FROM prodotti WHERE IDprodotto='".$_SESSION['IDprodotto']."'";
-			$result=mysqli_query($con,$select);
-			$row = mysqli_fetch_assoc($result);
-			$voto=($row['Voto']*$row['NumVoti']+$rate)/($row['NumVoti+1']);
-			$reg= 'Recensione Ok!';
-			$row['NumVoti']+=1;
-			$query="UPDATE `prodotti` SET `Voto` = '".$rate."', NumVoti='".$row['NumVoti']."'";
-			$res = mysqli_query($con,$query);
+			$rowcount = mysqli_num_rows($res);
+			if($rowcount>0)
+				{
+				$row = mysqli_fetch_assoc($res);
+				if($row['Votato']==1)
+					$reg='Hai già votato questo prodotto';
+				else
+					{
+					$rate=$_POST['star'];
+					$select = "SELECT Voto, NumVoti FROM prodotti WHERE ID='".$_SESSION['IDprodotto']."'";
+					$result=mysqli_query($con,$select);
+					$row = mysqli_fetch_assoc($result);
+					$voto=($row['Voto']*$row['NumVoti']+$rate)/($row['NumVoti']+1);
+					$reg= 'Recensione Ok!';
+					$row['NumVoti']+=1;
+					$query="UPDATE `prodotti` SET `Voto` = '".$voto."', NumVoti='".$row['NumVoti']."' WHERE ID='".$_SESSION['IDprodotto']."'";
+					$res = mysqli_query($con,$query);
+					$query = "UPDATE acquistato SET Votato=1 WHERE IDprodotto='".$_SESSION['IDprodotto']."' AND `IDutente` = '".$_SESSION['id']."'";
+					$res = mysqli_query($con,$query);
+					}
+				}
+				else 
+					$reg='Non hai acquistato questo prodotto, quindi non puoi recensirlo!';
 			}
-		else 
-			$reg= 'Non hai acquistato questo prodotto, quindi non puoi recensirlo!';
 		}
-	//echo "<script type='text/javascript'>alert('$reg');</script>";
-	//header("refresh:0; url=prod.php");
+	echo "<script type='text/javascript'>alert('$reg');</script>";
+	header("refresh:0; url=prod.php");
 	mysqli_close($con);	
 	include('../Templates/Footer.php');
 ?>
