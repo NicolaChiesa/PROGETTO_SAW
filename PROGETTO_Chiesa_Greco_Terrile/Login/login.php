9b51225382d15159ac6ed6bb60a33219e3234dc4
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-06 18:24:44 +0100

    Update login.php

--- PROGETTO_Chiesa_Greco_Terrile/Login/login.php
+++ PROGETTO_Chiesa_Greco_Terrile/Login/login.php
@@ -6,22 +6,25 @@
 	$email=mysqli_real_escape_string($con, $email);
 	$query = "SELECT Password, Nome, Cognome, ID FROM utenti WHERE Mail='".$email."'";
 	$res=mysqli_query($con,$query);
-	$row = mysqli_fetch_assoc($res);
-	if(password_verify($pass,$row['Password'])){
-		$_SESSION['nome']=$row['Nome'];
-		$_SESSION['cognome']=$row['Cognome'];
-		$_SESSION['id']=$row['ID'];
-		$_SESSION['Registrated'] = "true";
-		$_SESSION['email']= $_POST['email'];
-		if(isset($_SESSION['carrello']))
-			{
-			$quer="UPDATE `acquisto` SET `IDutente` = '".$_SESSION['id']."' WHERE `NumSessione` = '".session_id()."'";
-			$re = mysqli_query($con,$quer);
+	if($res!=false)
+	{
+		$row = mysqli_fetch_assoc($res);
+		if(password_verify($pass,$row['Password'])){
+			$_SESSION['nome']=$row['Nome'];
+			$_SESSION['cognome']=$row['Cognome'];
+			$_SESSION['id']=$row['ID'];
+			$_SESSION['Registrated'] = "true";
+			$_SESSION['email']= $_POST['email'];
+			if(isset($_SESSION['carrello']))
+				{
+				$quer="UPDATE `acquisto` SET `IDutente` = '".$_SESSION['id']."' WHERE `NumSessione` = '".session_id()."'";
+				$re = mysqli_query($con,$quer);
+				}
+			header("Location: ../Core/PaginaPrincipale.php");
 			}
-		header("Location: ../Core/PaginaPrincipale.php");
-		}
-	else
-		echo"ERRORE";
+		else
+			echo"ERRORE";
+			}
 	mysqli_close($con);
 ?>
 
