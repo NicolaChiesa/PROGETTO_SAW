c9e3490aa348bafc934340e9f8ae63c6196d096b
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-02-06 19:35:05 +0100

    Update durlindana.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
@@ -47,14 +47,6 @@
       <div class="col mt-5">
           <?php
             echo'
-            <div class="container py-5 my-5">
-              <div class="row align-items-md-stretch">
-                <div class="col"></div>
-                <div class="col-md-6">
-                  <div class="h-100 p-5 bg-light border border-3 border-danger rounded-3">
-                    <p class="testoBase"> su '.$row['NumVoti'].' voti la media di voto è: '.$row['Voto'].'</p>
-                  </div>
-
                 <div class="bg-light border">
                   <p class="testoBase"> su '.$row['NumVoti'].' voti la media di voto è: '.$row['Voto'].'</p>
                 </div>
