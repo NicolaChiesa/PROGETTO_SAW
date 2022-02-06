776f80d34809a6b4e0f097f705aadc746392e009
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-06 22:30:17 +0100

    Update Footer.php

--- PROGETTO_Chiesa_Greco_Terrile/Templates/Footer.php
+++ PROGETTO_Chiesa_Greco_Terrile/Templates/Footer.php
@@ -12,9 +12,20 @@
         </ul>
       </div>
       <?php    
-      if(isset($_SESSION['Registrated']) && $_SESSION['Registrated'] == "true"){
-      echo'
-      <div class="col-4 offset-1 mt-3">
+        if(isset($_SESSION['Registrated'])){
+      echo'<div class="col-4 offset-1 mt-3">
+            <form id="myform" action="../Newsletter/IscrivitiNewsletter.php" method="POST" >
+            <h5 class="text-light">Iscriviti alla nostra newsletter!</h5>
+            <p class="text-light">Tutti i mesi fantastiche novità!</p>
+            <div class="d-flex w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden"></label>
+              <input type="submit" placeholder="Iscriviti"></button>
+            </div>
+          </form>
+      </div>
+    ';}
+    else{     
+    echo'<div class="col-4 offset-1 mt-3">
             <form id="myform" action="../Newsletter/IscrivitiNewsletter.php" method="POST" >
             <h5 class="text-light">Iscriviti alla nostra newsletter!</h5>
             <p class="text-light">Tutti i mesi fantastiche novità!</p>
@@ -25,22 +36,11 @@
             </div>
           </form>
       </div>
-    </div>';}
-    else{
-        echo'           
-        <form id="myform" action="../Newsletter/IscrivitiNewsletter.php" method="POST" >
-            <h5 class="text-light">Iscriviti alla nostra newsletter!</h5>
-            <p class="text-light">Tutti i mesi fantastiche novità!</p>
-            <div class="d-flex w-100 gap-2">
-              <label for="newsletter1" class="visually-hidden"></label>
-              <input type="submit" placeholder="Iscriviti"></button>
-            </div>
-          </form>
-      </div>
-    </div>';
+    ';
+
     }
   ?>
-  
+  </div>
     <div class="d-flex justify-content-between py-1 border-top text-light">
       <p>&copy; 2021 Company, Only Mytich, tutti i diritti sono riesrvati</p>
       <ul class="list-unstyled d-flex">
