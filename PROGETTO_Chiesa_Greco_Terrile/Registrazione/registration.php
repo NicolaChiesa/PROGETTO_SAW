f676c2338472e957b4cfc593237442eaab74d000
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-06 18:47:31 +0100

    Update registration.php

--- PROGETTO_Chiesa_Greco_Terrile/Registrazione/registration.php
+++ PROGETTO_Chiesa_Greco_Terrile/Registrazione/registration.php
@@ -7,55 +7,62 @@
 	include('../connessione.php');
 	$quer="SELECT* FROM utenti WHERE Mail='".$_POST['email']."'";
 	$re = mysqli_query($con,$quer);
-	$row = mysqli_fetch_assoc($re);
-	$num = mysqli_affected_rows($con);
-	mysqli_free_result($re);
+	if($re!=false)
+		{
+		$row = mysqli_fetch_assoc($re);
+		$num = mysqli_affected_rows($con);
+		mysqli_free_result($re);
 	
-	if($_POST['pass']==""||$_POST['confirm']==""||$_POST['lastname']==""||$_POST['firstname']=="")
-		echo"<h1>Alcuni dati non sno stati inseriti</h1>";
-	else if($_POST['pass']!=$_POST['confirm'])
-		echo"<h1>Le due password non coincidono</h1>";
-	else if($num==1)
-		echo "<h1>email già usata</h1>";
+		if($_POST['pass']==""||$_POST['confirm']==""||$_POST['lastname']==""||$_POST['firstname']=="")
+			$reg='Alcuni dati non sno stati inseriti';
+		else if($_POST['pass']!=$_POST['confirm'])
+			$reg='Le due password non coincidono';
+		else if($num==1)
+			$reg= 'Email già usata';
 	
 	// se tutto è andato a buon fine 
-	else{
-		$_SESSION['Registrated'] = "true";
-		$_SESSION['nome']=$_POST['firstname'];
-		$_SESSION['cognome']=$_POST['lastname'];
-		$_SESSION['email']=$_POST['email'];
-		$pass=trim($_POST['pass']);
-		$lastname=htmlspecialchars($_POST['lastname']);
-		$firstname=htmlspecialchars($_POST['firstname']);
-		$email=htmlspecialchars($_POST['email']);
-		$pass=mysqli_real_escape_string($con, $pass);
-		$lastname=mysqli_real_escape_string($con, $lastname);
-		$firstname=mysqli_real_escape_string($con, $firstname);
-		$email=mysqli_real_escape_string($con, $email);
-		$pass=password_hash($pass,PASSWORD_BCRYPT);
-		$query ="INSERT INTO utenti (Nome, Cognome, Password, Mail) VALUES ('".$firstname."', '".$lastname."', '".$pass."', '".$email."')";
-		$res = mysqli_query($con,$query);
-		$query = "SELECT ID FROM utenti WHERE Mail='".$email."'";
-		$res=mysqli_query($con,$query);
-		if($res!=false)
-			{
-			$row = mysqli_fetch_assoc($res);
-			$_SESSION['id']=$row['ID'];
-			}
-		if(isset($_SESSION['carrello']))
-			{
-			$quer="UPDATE `acquisto` SET `IDutente` = '".$_SESSION['id']."' WHERE `NumSessione` = '".session_id()."'";
-			$re = mysqli_query($con,$quer);
-			$num = mysqli_affected_rows($con);
-			echo $num;
-			}
-		if(isset($_POST['Subscription'])){
-			header("Location: ../Newsletter/IscrivitiNewsletter.php");
-		}
 		else{
-			header("Location: ../Core/PaginaPrincipale.php");
+			$reg='Utente inserito con successo';
+			$_SESSION['Registrated'] = "true";
+			$_SESSION['nome']=$_POST['firstname'];
+			$_SESSION['cognome']=$_POST['lastname'];
+			$_SESSION['email']=$_POST['email'];
+			$pass=trim($_POST['pass']);
+			$lastname=htmlspecialchars($_POST['lastname']);
+			$firstname=htmlspecialchars($_POST['firstname']);
+			$email=htmlspecialchars($_POST['email']);
+			$pass=mysqli_real_escape_string($con, $pass);
+			$lastname=mysqli_real_escape_string($con, $lastname);
+			$firstname=mysqli_real_escape_string($con, $firstname);
+			$email=mysqli_real_escape_string($con, $email);
+			$pass=password_hash($pass,PASSWORD_BCRYPT);
+			$query ="INSERT INTO utenti (Nome, Cognome, Password, Mail) VALUES ('".$firstname."', '".$lastname."', '".$pass."', '".$email."')";
+			$res = mysqli_query($con,$query);
+			$query = "SELECT ID FROM utenti WHERE Mail='".$email."'";
+			$res=mysqli_query($con,$query);
+			if($res!=false)
+				{
+				$row = mysqli_fetch_assoc($res);
+				$_SESSION['id']=$row['ID'];
+				}
+			if(isset($_SESSION['carrello']))
+				{
+				$quer="UPDATE `acquisto` SET `IDutente` = '".$_SESSION['id']."' WHERE `NumSessione` = '".session_id()."'";
+				$re = mysqli_query($con,$quer);
+				$num = mysqli_affected_rows($con);
+				echo $num;
+				}
+			if(isset($_POST['Subscription'])){
+				echo "<script type='text/javascript'>alert('$reg');</script>";
+				header("refresh:0; url=../Newsletter/IscrivitiNewsletter.php");
+			}
+			else{
+			echo "<script type='text/javascript'>alert('$reg');</script>";
+			header("refresh:0; url=../Core/PaginaPrincipale.php");
+			}	
 		}
 		mysqli_close($con);
+		include('../Templates/Footer.php');
 	}
 
 	
