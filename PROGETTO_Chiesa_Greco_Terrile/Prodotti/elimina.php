648c56875baac933803ab0199671da1c0832cee7
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-06 18:20:13 +0100

    Update elimina.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/elimina.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/elimina.php
@@ -3,13 +3,16 @@
 	include('../connessione.php');
 	$query = "SELECT prezzo, quantita FROM prodotti WHERE ID='".$_GET['ID']."'";
 	$res=mysqli_query($con,$query);
-	$row = mysqli_fetch_assoc($res);
-	$row['quantita']+=$_GET['num'];
-	$quer="UPDATE `prodotti` SET `quantita` = '".$row['quantita']."' WHERE `ID` = '".$_GET['ID']."'";
-	$re = mysqli_query($con,$quer);
-	$query="DELETE FROM `acquisto` WHERE `acquisto`.`NumSessione` = '".session_id()."' AND IDprodotto='".$_GET['ID']."'";
-	$res=mysqli_query($con,$query);
-	$inserimento='Prodotto eliminato con successo :)';
+	if(res!=false)
+		{
+		$row = mysqli_fetch_assoc($res);
+		$row['quantita']+=$_GET['num'];
+		$quer="UPDATE `prodotti` SET `quantita` = '".$row['quantita']."' WHERE `ID` = '".$_GET['ID']."'";
+		$re = mysqli_query($con,$quer);
+		$query="DELETE FROM `acquisto` WHERE `acquisto`.`NumSessione` = '".session_id()."' AND IDprodotto='".$_GET['ID']."'";
+		$res=mysqli_query($con,$query);
+		$inserimento='Prodotto eliminato con successo :)';
+		}
 	echo "<script type='text/javascript'>alert('$inserimento');</script>";
 	header("refresh:0; url=finisci.php");
 	mysqli_close($con);	
