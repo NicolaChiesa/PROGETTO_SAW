440f61ca3ccc7739ad55939ffc233a7350dbc999
NicolaChiesa <89846244+NicolaChiesa@users.noreply.github.com>
2022-02-06 20:27:34 +0100

    Delete Ricerca.php

--- PROGETTO_Chiesa_Greco_Terrile/Ricerca.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('../Templates/Header.php');
-    include('../connessione.php');
-    $p = '';
-    $query = mysqli("SELECT ID FROM prodotti WHERE Nome LIKE '% '% ");
-    while($a = mysqli_fetch_array($query)){
-        echo $a['ID'];
-    }
