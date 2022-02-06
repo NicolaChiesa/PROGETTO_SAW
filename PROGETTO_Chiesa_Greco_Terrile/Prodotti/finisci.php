fe8e0ef3149945c33f0670d86c0588d1a3a31ede
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-06 10:23:18 +0100

    Update finisci.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/finisci.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/finisci.php
@@ -1,24 +1,77 @@
 <?php
 	include('../Templates/Header.php');
 	include('../connessione.php');
-	$query = "SELECT * FROM acquisto WHERE NumSessione='".session_id()."'";
-	$res=mysqli_query($con,$query);
+	$flag=0;
+	$sommadur="SELECT SUM(Quantita) AS sommadur FROM acquisto WHERE NumSessione='".session_id()."' AND IDprodotto='1'";
+	$res=mysqli_query($con,$sommadur);
 	$row = mysqli_fetch_assoc($res);
 	$rowcount=mysqli_num_rows($res);
-	if($rowcount>0)
+	if($rowcount>0 && $row['sommadur']>0)
 		{
-		echo 'Stai acquistando dal nostro sito: '.$row['NomeProdotto'].' alla modica cifra di: '.$row['Prezzo'];
-		$prezzo=$row['Prezzo'];
-	while($row=mysqli_fetch_assoc($res))
+		echo 'Stai acquistando dal nostro sito: '.$row['sommadur'].' Durlindana';
+		echo'<a href="elimina.php?ID=1&num='.$row['sommadur'].'">Vuoi eliminare il prodotto?</a>';
+		$flag=1;
+		}
+
+	$sommamart="SELECT SUM(Quantita) AS sommamart FROM acquisto WHERE NumSessione='".session_id()."' AND IDprodotto='3'";
+	$res=mysqli_query($con,$sommamart);
+	$row = mysqli_fetch_assoc($res);
+	$rowcount=mysqli_num_rows($res);
+	if($rowcount>0 && $row['sommamart']>0)
 		{
-		echo' acquisti anche: '.$row['NomeProdotto'].' alla modica cifra di: '.$row['Prezzo'];
-		$prezzo+=$row['Prezzo'];
+		echo 'Stai acquistando dal nostro sito: '.$row['sommamart'].' Mjolnir';
+		echo'<a href="elimina.php?ID=3&num='.$row['sommamart'].'">Vuoi eliminare il prodotto?</a>';
+		$flag=1;
 		}
-		echo ' per una spesa totale di: '.$prezzo;
-		echo'<a href="concludi.php"<button id="pulsante"><i class="icon-shopping-cart"></i> Vai al pagamento</button></a>';
+
+	$sommaexc="SELECT SUM(Quantita) AS sommaexc FROM acquisto WHERE NumSessione='".session_id()."' AND IDprodotto='2'";
+	$res=mysqli_query($con,$sommaexc);
+	$row = mysqli_fetch_assoc($res);
+	$rowcount=mysqli_num_rows($res);
+	if($rowcount>0 && $row['sommaexc']>0)
+		{
+		echo 'Stai acquistando dal nostro sito: '.$row['sommaexc'].' Excalibur';
+		echo'<a href="elimina.php?ID=2&num='.$row['sommaexc'].'">Vuoi eliminare il prodotto?</a>';
+		$flag=1;
 		}
-	else 
-		echo 'Carello vuoto';
+
+	$sommaol="SELECT SUM(Quantita) AS sommaol FROM acquisto WHERE NumSessione='".session_id()."' AND IDprodotto='4'";
+	$res=mysqli_query($con,$sommaol);
+	$row = mysqli_fetch_assoc($res);
+	$rowcount=mysqli_num_rows($res);
+	if($rowcount>0 && $row['sommaol']>0)
+		{
+		echo 'Stai acquistando dal nostro sito: '.$row['sommaol'].' Olifante';
+		echo'<a href="elimina.php?ID=4&num='.$row['sommaol'].'">Vuoi eliminare il prodotto?</a>';
+		$flag=1;
+		}
+
+	$sommapom="SELECT SUM(Quantita) AS sommapom FROM acquisto WHERE NumSessione='".session_id()."' AND IDprodotto='5'";
+	$res=mysqli_query($con,$sommapom);
+	$row = mysqli_fetch_assoc($res);
+	$rowcount=mysqli_num_rows($res);
+	if($rowcount>0 && $row['sommapom']>0)
+		{
+		echo 'Stai acquistando dal nostro sito: '.$row['sommapom'].' Pomo della Discordia';
+		echo'<a href="elimina.php?ID=5&num='.$row['sommapom'].'">Vuoi eliminare il prodotto?</a>';
+		$flag=1;
+		}
+
+	$sommaruy="SELECT SUM(Quantita) AS sommaruy FROM acquisto WHERE NumSessione='".session_id()."' AND IDprodotto='6'";
+	$res=mysqli_query($con,$sommaruy);
+	$row = mysqli_fetch_assoc($res);
+	$rowcount=mysqli_num_rows($res);
+	if($rowcount>0 && $row['sommaruy']>0)
+		{
+		echo 'Stai acquistando dal nostro sito: '.$row['sommaruy'].' Ruyi Jingu Bang';
+		echo'<a href="elimina.php?ID=6&num='.$row['sommaruy'].'">Vuoi eliminare il prodotto?</a>';
+		$flag=1;
+		}
+
+	if($flag==0)
+		{
+		echo'Carrello vuoto';
+		}
 	mysqli_close($con);	
 	include('../Templates/Footer.php');
 ?>
