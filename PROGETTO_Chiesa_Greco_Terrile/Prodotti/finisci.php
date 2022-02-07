532db8838b5fd5485e96dd659b6fa2ca2a70924a
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-07 17:33:07 +0100

    Update finisci.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/finisci.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/finisci.php
@@ -141,7 +141,7 @@
 				<div class="card border-dark h-100">
 				<img src="../Immagini/RuyiJinguBang2.jpg" class="card-img-top immagini" alt="Pomo della discordia">
 					<div class="card-body">
-						<h5 class="card-title titoloCard">Pomo d oro</h5>
+						<h5 class="card-title titoloCard">RuyiJinguBang</h5>
 						<p class="card-text testoCard">Stai acquistando '.$sommaruy.' RuyiJinguBang alla modica cifra di: '.$prezzoruy.' euro</p>
 						<a href="elimina.php?ID=6&num='.$sommaruy.'" class="btn btn-outline-light">Elimina dal carrello</a>
 					</div>
