48991e1ea51b8018c0420e2808f30a56975040d4
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2022-02-07 12:57:43 +0100

    Update durlindana.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/durlindana.php
@@ -56,7 +56,9 @@
 
       <div class="col mt-5">
         <div class="text-end">
-          <a href="carrello.php" <button type="button" class="btn btn-outline-light me-2">Aggiungi al carrello</button></a>
+          <form style="display: inline" action="carrello.php" method="POST">
+            <input type="submit" class="btn btn-outline-light me-2">Aggiungi al carrello</button>
+          </form>
         </div>
       </div>
     </div>
