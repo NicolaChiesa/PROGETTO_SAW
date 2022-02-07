8397ccceec415a8d029de37008fea955fa37a671
Michelegre <97965998+Michelegre@users.noreply.github.com>
2022-02-07 17:31:30 +0100

    Update finisci.php

--- PROGETTO_Chiesa_Greco_Terrile/Prodotti/finisci.php
+++ PROGETTO_Chiesa_Greco_Terrile/Prodotti/finisci.php
@@ -139,7 +139,7 @@
 			echo'
 			<div class="col">
 				<div class="card border-dark h-100">
-				<img src="../Immagini/pomo.jpg" class="card-img-top immagini" alt="Pomo della discordia">
+				<img src="../Immagini/RuyiJinguBang2.jpg" class="card-img-top immagini" alt="Pomo della discordia">
 					<div class="card-body">
 						<h5 class="card-title titoloCard">Pomo d oro</h5>
 						<p class="card-text testoCard">Stai acquistando '.$sommaruy.' Pomo della discordia alla modica cifra di: '.$prezzoruy.' euro</p>
